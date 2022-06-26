c761664b777902d21dc87bcaf620f8b8e8f88d47
lpirito <lucasignaciopirito@gmail.com>
2022-06-26 16:20:14 -0300

    renamed entity and moved annotation

--- src/main/java/com/lpirito/devscrapper/controller/DevscrapperController.java
+++ src/main/java/com/lpirito/devscrapper/controller/DevscrapperController.java
@@ -2,10 +2,8 @@ package com.lpirito.devscrapper.controller;
 
 import com.lpirito.devscrapper.entity.ComputrabajoEntity;
 import com.lpirito.devscrapper.entity.LinkedinEntity;
-import com.lpirito.devscrapper.entity.ScrapperEntity;
+import com.lpirito.devscrapper.entity.JobPostEntity;
 import com.lpirito.devscrapper.service.DevscrapperService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,15 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.util.ArrayList;
 
-@RestController
 @RequestMapping("/")
+@RestController
 public class DevscrapperController {
 
     @Autowired
     DevscrapperService service;
 
     @GetMapping(value = "/jobs/arg", produces = "application/json")
-    public ArrayList<ScrapperEntity> getJobList() throws IOException {
+    public ArrayList<JobPostEntity> getJobList() throws IOException {
         return service.getJobList();
     }
 
