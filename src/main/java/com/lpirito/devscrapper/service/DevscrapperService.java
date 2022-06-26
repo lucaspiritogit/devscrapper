54850333ed986098f7276c36121178a694decfe7
lpirito <lucasignaciopirito@gmail.com>
2022-06-26 16:22:22 -0300

    improved computrabajo selection

--- src/main/java/com/lpirito/devscrapper/service/DevscrapperService.java
+++ src/main/java/com/lpirito/devscrapper/service/DevscrapperService.java
@@ -2,42 +2,32 @@ package com.lpirito.devscrapper.service;
 
 import com.lpirito.devscrapper.entity.ComputrabajoEntity;
 import com.lpirito.devscrapper.entity.DocumentEntity;
+import com.lpirito.devscrapper.entity.JobPostEntity;
 import com.lpirito.devscrapper.entity.LinkedinEntity;
-import com.lpirito.devscrapper.entity.ScrapperEntity;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 
-import static com.lpirito.devscrapper.helper.DocumentHelper.getDocument;
+import static com.lpirito.devscrapper.helper.DocumentConnector.getDocument;
 
 @Service
 public class DevscrapperService {
 
     static String computrabajoUrl = "https://www.computrabajo.com.ar/trabajo-de-desarrollador?p=";
-    static String linkedinUrl = "https://www.linkedin.com/jobs/search/?keywords=desarrollador&location=Argentina";
+    static String linkedinUrl = "https://www.linkedin.com/jobs/search/?keywords=desarrollador";
 
-    public ArrayList<ScrapperEntity> getJobList() {
+    public ArrayList<JobPostEntity> getJobList() {
 
-        ArrayList<ScrapperEntity> response = new ArrayList<>();
+        ArrayList<JobPostEntity> response = new ArrayList<>();
         response.addAll(computrabajoPosts());
         response.addAll(linkedinPosts());
-        responseIndexer(response);
 
-        Collections.shuffle(response);
         return response;
     }
 
-    private void responseIndexer(ArrayList<ScrapperEntity> response) {
-        int index = 0;
-        for (ScrapperEntity jobPostEntity : response) {
-            jobPostEntity.setId(index++);
-        }
-    }
 
     // handles individual job postings of computrabajo
     public ArrayList<ComputrabajoEntity> computrabajoPosts() {
@@ -47,18 +37,27 @@ public class DevscrapperService {
             computrabajoUrl = "https://www.computrabajo.com.ar/trabajo-de-desarrollador?p=" + numOfPages;
             numOfPages++;
             Element computrabajoHtmlDocument = getDocument(computrabajoUrl);
-            Elements computrabajoJobPostsTitles = computrabajoHtmlDocument.select("a.js-o-link.fc_base");
+            Elements computrabajoJobPosts = computrabajoHtmlDocument.select(".w100.bClick");
+
             // where the job post came from
             DocumentEntity computrabajoOrigin = new DocumentEntity(0, "computrabajo", computrabajoUrl);
 
-            for (Element computrabajoTitle : computrabajoJobPostsTitles) {
-                ComputrabajoEntity computrabajoJobPost = new ComputrabajoEntity();
-                computrabajoJobPost.setJobTitle(computrabajoTitle.text());
-                String linkToJobPost = computrabajoTitle.attr("href");
-                String url = "https://www.computrabajo.com.ar".concat(linkToJobPost);
-                computrabajoJobPost.setUrl(url);
-                computrabajoJobPost.setOrigin(computrabajoOrigin);
-                computrabajoJobPostsArray.add(computrabajoJobPost);
+            for (Element computrabajoJobPost : computrabajoJobPosts) {
+                ComputrabajoEntity computrabajoEntity = new ComputrabajoEntity();
+
+                String jobPostTitle = computrabajoJobPost.select("a.js-o-link.fc_base").text();
+                computrabajoEntity.setJobTitle(jobPostTitle);
+
+                String jobPostHref = computrabajoJobPost.select("a.js-o-link.fc_base").attr("href");
+                String url = "https://www.computrabajo.com.ar".concat(jobPostHref);
+                computrabajoEntity.setUrl(url);
+
+                String wasPosted = computrabajoJobPost.select(".fs13.fc_aux").text();
+                computrabajoEntity.setWasPosted(wasPosted);
+
+                computrabajoEntity.setOrigin(computrabajoOrigin);
+
+                computrabajoJobPostsArray.add(computrabajoEntity);
             }
         }
         return computrabajoJobPostsArray;
