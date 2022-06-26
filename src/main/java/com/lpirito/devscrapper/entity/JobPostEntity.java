88e916b4eb7be2e5f057eda42631d29282552557
lpirito <lucasignaciopirito@gmail.com>
2022-06-26 16:18:15 -0300

    added new fields

--- src/main/java/com/lpirito/devscrapper/entity/JobPostEntity.java
+++ src/main/java/com/lpirito/devscrapper/entity/JobPostEntity.java
@@ -3,20 +3,21 @@ package com.lpirito.devscrapper.entity;
 import lombok.*;
 import org.springframework.stereotype.Component;
 
+
 @Getter
 @Setter
 @ToString
-@Component
 @NoArgsConstructor
 @AllArgsConstructor
-public class ScrapperEntity{
+@Component
+public class JobPostEntity {
 
     private int id;
     private String jobTitle;
     private String url;
     private String company;
-    private String description;
     private String location;
+    private String wasPosted;
     private DocumentEntity origin;
 
 }
